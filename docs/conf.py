90b20f1282cc60e55d2f01b433662b5af6875df8
whitequark <whitequark@whitequark.org>
2018-01-12 03:44:59 +0000

    Mock out PyQt5 for readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,21 @@
 
 import sys, os
 import sphinx_rtd_theme
+from mock import Mock as MagicMock
 
+# Configure our load path
 sys.path.insert(0, os.path.abspath('..'))
 
+# Mock out C dependencies for readthedocs
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['PyQt5', 'PyQt5.QtCore']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+# Configure Sphinx
 extensions = ['sphinx.ext.autosummary', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 templates_path = ['_templates']
 autosummary_generate = True
